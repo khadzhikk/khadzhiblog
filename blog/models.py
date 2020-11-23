5cfb09599daae25fba446ce1fad22487d2754d2f
khadzhikk <khadzhikk@pop-os.localdomain>
2020-11-23 20:44:21 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_date_add=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
